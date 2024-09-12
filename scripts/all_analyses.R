9aa30e71cb4db4d38809eff3f2e3ac6350ede06a
Jocelyn Lauzon <jocelyn.lauzon@gmail.com>
2024-09-12 15:33:06 -0400

    Edit script.

--- scripts/all_analyses.R
+++ scripts/all_analyses.R
@@ -162,7 +162,7 @@ colnames(models.H2.table) <-
     "J-048","J-076","E-045","J-092","E-005","J-068","adj.R2","df","LL","AICc","ΔAICc","weight")
 
 # table output for Appendix 2
-write.csv2(models.H2.table, file = "data/H2_model_exploration.csv", row.names = F)
+write.csv2(models.H2.table, file = "data/H2_all_models.csv", row.names = F)
 
 ## averaging ----
 # not used in article, but done just for data exploration
@@ -309,7 +309,7 @@ colnames(models.H3.table) <-
 "E-045:J-092:D2","J-067:E-045:J-092:D2","adj.R2","df","LL","AICc","ΔAICc","weight")
 
 # Output for Appendix 3
-write.csv2(models.H3.table, file = "data/H3_model_exploration.csv", row.names = F)
+write.csv2(models.H3.table, file = "data/H3_all_models.csv", row.names = F)
 
 ## averaging ----
 modH3 <- model.avg(models.H3, subset = delta <= 2, fit = T,  data = data_lb.d.c)
